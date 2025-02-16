601374c78273e29c408622ea4675dbd4b87dd5db
rakslice <rakslice@gmail.com>
2025-02-15 22:43:17 -0800

    SheepShaver verison of the main_windows.cpp change for multimonitor

--- SheepShaver/src/Windows/main_windows.cpp
+++ SheepShaver/src/Windows/main_windows.cpp
@@ -126,6 +126,10 @@ extern void exit_emul_ppc(void);
 sigsegv_return_t sigsegv_handler(sigsegv_info_t *sip);
 static LRESULT CALLBACK low_level_keyboard_hook(int nCode, WPARAM wParam, LPARAM lParam);
 
+#if SDL_VERSION_ATLEAST(2,0,0)
+	extern SDL_Window * get_main_sdl_window();
+#endif
+
 
 /*
  *  Return signal stack base
@@ -806,9 +810,11 @@ void SheepMem::Exit(void)
 #else
 #include <SDL_syswm.h>
 #endif
-extern SDL_Window *sdl_window;
 HWND GetMainWindowHandle(void)
 {
+#if SDL_VERSION_ATLEAST(2,0,0)
+	SDL_Window * sdl_window = get_main_sdl_window();
+#endif
 	if (!sdl_window) {
 		return NULL;
 	}
@@ -877,6 +883,9 @@ bool ChoiceAlert(const char *text, const char *pos, const char *neg)
  */
 static LRESULT CALLBACK low_level_keyboard_hook(int nCode, WPARAM wParam, LPARAM lParam)
 {
+#if SDL_VERSION_ATLEAST(2,0,0)
+	SDL_Window * sdl_window = get_main_sdl_window();
+#endif
 	// Not a relevant event, immediately pass it on
 	if (nCode != HC_ACTION)
         return CallNextHookEx(keyboard_hook, nCode, wParam, lParam);
